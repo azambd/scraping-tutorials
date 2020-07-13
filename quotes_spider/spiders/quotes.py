c568d0ca5ad765aaefd8364ad319f4beec2a8d4f
Faruque A <engrfazam@gmail.com>
2020-07-14 00:46:34 +0600

    Using CSS Selector

--- quotes_spider/spiders/quotes.py
+++ quotes_spider/spiders/quotes.py
@@ -10,16 +10,22 @@ class QuotesSpider(scrapy.Spider):
     )
 
     def parse(self, response):
-        quotes = response.xpath('//*[@class="quote"]')
+        # quotes = response.xpath('//*[@class="quote"]')
+        # for quote in quotes:
+        #     text = quote.xpath('.//*[@class="text"]/text()').extract_first()
+        #     author = quote.xpath('.//*[@itemprop="author"]/text()').extract_first()
+        #     tags = quote.xpath('.//*[@itemprop="keywords"]/@content').extract_first()
+        #Using CSS Selector:
+        quotes = response.css('div.quote')
         for quote in quotes:
-            text = quote.xpath('.//*[@class="text"]/text()').extract_first()
-            author = quote.xpath('.//*[@itemprop="author"]/text()').extract_first()
-            tags = quote.xpath('.//*[@itemprop="keywords"]/@content').extract_first()
+            text = quote.css('span.text::text').extract_first()
+            author = quote.css('small[itemprop="author"]::text').extract_first()
+            tags = quote.css('[itemprop="keywords"]::attr(content)').extract_first()
 
             yield{'Text': text,
                   'Author': author,
                   'Tags': tags}
 
-        next_page_url = response.xpath('//*[@class="next"]/a/@href').extract_first()
+        next_page_url = response.css('.next a::attr(href)').extract_first()
         absolute_next_page_url = response.urljoin(next_page_url)
         yield scrapy.Request(absolute_next_page_url)
